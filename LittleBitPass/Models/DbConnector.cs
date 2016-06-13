cb669fe6f315c5a8dc92c1f3a33d9257b92e06e1
Jaap-Jan de Wit <jipjan@gmail.com>
2016-06-13 14:00:23 +0200

    add class for communicating with mysql db

---
+++ LittleBitPass/Models/DbConnector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace LittleBitPass
+{
+	public class DbConnector
+	{
+		// Singleton, prevent multiple DB connections.
+		public static readonly DbConnector Instance = new DbConnector();
+
+		public MySqlConnection Connection;
+
+		private DbConnector() {
+			Connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["DbConnString"].ConnectionString);
+		}
+
+	}
+}
+
