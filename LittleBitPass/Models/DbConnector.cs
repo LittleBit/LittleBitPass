56def2f42c4284228f67474bc187563a4d64e758
Jaap-Jan de Wit <jipjan@gmail.com>
2016-06-14 15:30:57 +0200

    remove mysql and use PostgreSQL, added basic connection logic

--- LittleBitPass/Models/DbConnector.cs
+++ LittleBitPass/Models/DbConnector.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using MySql.Data.MySqlClient;
+using Devart.Data.PostgreSql;
 using System.Configuration;
+using System.Threading.Tasks;
+using System.Data.Common;
 
 namespace LittleBitPass
 {
@@ -9,12 +11,21 @@ namespace LittleBitPass
 		// Singleton, prevent multiple DB connections.
 		public static readonly DbConnector Instance = new DbConnector();
 
-		public MySqlConnection Connection;
+		public PgSqlConnection Connection;
 
-		private DbConnector() {
-			Connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["DbConnString"].ConnectionString);
+		DbConnector() {
+			Connection = new PgSqlConnection(ConfigurationManager.ConnectionStrings["DbConnString"].ConnectionString);
+			Connection.Open();
 		}
 
+		public PgSqlDataReader RunQuery(string query) {
+			var test = new PgSqlCommand(query, Connection);
+			return test.ExecuteReader();
+		}
+
+		public Task<DbDataReader> RunQueryAsync(string query) {
+			var test = new PgSqlCommand(query, Connection);
+			return test.ExecuteReaderAsync();
+		}
 	}
 }
-
