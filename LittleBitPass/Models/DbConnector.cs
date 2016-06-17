63490d958f5dbeeb26c5df85b45ab9ae7c18d2fc
Jaap-Jan <jipjan@gmail.com>
2016-06-17 20:11:30 +0200

    Add documentation
    added documentation and better variable names

--- LittleBitPass/Models/DbConnector.cs
+++ LittleBitPass/Models/DbConnector.cs
@@ -6,30 +6,40 @@ using System.Data.Common;
 
 namespace LittleBitPass
 {
+	/// <summary>
+	/// This class provides the connection to the database and handles queries to return a datareader.
+	/// </summary>
 	public class DbConnector
 	{
 		// Singleton, prevent multiple DB connections.
 		public static readonly DbConnector Instance = new DbConnector();
 
-		public PgSqlConnection Connection;
+		internal PgSqlConnection Connection;
 
 		DbConnector() {
+			// Connection string can be found in the Web.config file
 			Connection = new PgSqlConnection(ConfigurationManager.ConnectionStrings["DbConnString"].ConnectionString);
-			try {				
-				Connection.Open();
-			} catch (Exception ex) {
-				Console.WriteLine (ex.Message);
-			}
+			Connection.Open();
 		}
 
+		/// <summary>
+		/// Runs the query sync against the database and returns a datareader.
+		/// </summary>
+		/// <returns>A datareader containing the result of the query.</returns>
+		/// <param name="query">Query.</param>
 		public PgSqlDataReader RunQuery(string query) {
-			var test = new PgSqlCommand(query, Connection);
-			return test.ExecuteReader();
+			var cmd = new PgSqlCommand(query, Connection);
+			return cmd.ExecuteReader();
 		}
 
+		/// <summary>
+		/// Runs the query sync against the database and returns a datareader.
+		/// </summary>
+		/// <returns>A task containing a datareader to access the result of the query.</returns>
+		/// <param name="query">Query.</param>
 		public Task<DbDataReader> RunQueryAsync(string query) {
-			var test = new PgSqlCommand(query, Connection);
-			return test.ExecuteReaderAsync();
+			var cmd = new PgSqlCommand(query, Connection);
+			return cmd.ExecuteReaderAsync();
 		}
 	}
 }
