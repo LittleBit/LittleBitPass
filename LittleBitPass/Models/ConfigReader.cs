2e877f301a907a3b74c325b9a0852fecab0ae6dd
BuildTools <unconfigured@null.spigotmc.org>
2016-08-27 00:29:38 +0200

    Config file done

--- LittleBitPass/Models/ConfigReader.cs
+++ LittleBitPass/Models/ConfigReader.cs
@@ -1,48 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace LittleBitPass
 {
 	public static class ConfigReader
 	{
-		public static ConfigFile Init()
+		private const string DB_NAME = "dbName", DB_USERNAME = "dbUsername", DB_PASSWORD = "dbPassword", DB_ADDRESS = "dbAddress";
+		private static readonly string[] ConfigKeys = new [] { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_ADDRESS };
+
+		public static void Init(Action<ConfigFile> ready)
 		{
-			return null;
-//			FindConfigFile(() =>
-//			{
-//				return new ConfigFile();
-//			}, () =>
-//			{
-//				return new ConfigFile();
-//			});
+			ParseConfigFile(configFile =>
+			{
+				ready(configFile);
+			}, msg =>
+			{
+				ready(new ConfigFile());
+			});
 		}
 
-		public static void FindConfigFile(Action<StringReader> found, Action notFound)
+		public static void ParseConfigFile(Action<ConfigFile> success, Action<string> fail)
 		{
 			const string ConfigFilePath = "config";
+			const char ConfigSeperator = ':';
 			if (File.Exists(ConfigFilePath))
 			{
-				File.OpenText(ConfigFilePath);
+				var dict = new Dictionary<string, string>();
+				var textReader = File.OpenText(ConfigFilePath);
+				var line = "";
+				while ((line = textReader.ReadLine()) != null) {
+					var kvp = line.Split(ConfigSeperator);
+					if (kvp.Length == 2) {
+						dict[kvp[0]] = kvp[1];
+					}
+					else {
+						// not a (valid) config entry
+					}
+				}
+				foreach (var cKey in ConfigKeys)
+				{
+					if (!dict.ContainsKey(cKey))
+					{
+						fail("Config file is missing " + cKey + "  - ");
+						return;
+					}
+				}
+				success(new ConfigFile(dict[DB_NAME],dict[DB_USERNAME],dict[DB_PASSWORD],dict[DB_ADDRESS]));
 			}
 			else {
-				notFound();
+				fail("Config file does not exist!");
 			}
 		}
 	}
 
 	public class ConfigFile
 	{
-		public readonly string DbUsername, DbPassword, DbAddress;
+		public readonly string DbName, DbUsername, DbPassword, DbAddress;
 
 		public ConfigFile()
 		{
-			DbUsername = "Fap me baby one more time";
-			DbPassword = "Will I Am, it's Britney b1tch!-";
-			DbAddress = "8.8.4.4";
+			DbName = "database";
+			DbUsername = "root";
+			DbPassword = "";
+			DbAddress = "localhost";
 		}
 
-		public ConfigFile(string dbUsername, string dbPassword, string dbAdress)
+		public ConfigFile(string dbName, string dbUsername, string dbPassword, string dbAdress)
 		{
+			DbName = dbName;
 			DbUsername = dbUsername;
 			DbPassword = dbPassword;
 			DbAddress = dbAdress;
