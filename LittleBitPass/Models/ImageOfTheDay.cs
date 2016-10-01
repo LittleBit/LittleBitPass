6a02821cd149b0fa0f14f482c1566ac3619e961f
Jaap-Jan de Wit <jipjan@gmail.com>
2016-10-02 00:27:48 +0200

    improved json object style to be compliant with default C# code style

--- LittleBitPass/Models/ImageOfTheDay.cs
+++ LittleBitPass/Models/ImageOfTheDay.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Net;
 using System.Timers;
 using Newtonsoft.Json;
@@ -23,9 +22,9 @@ namespace LittleBitPass.Models
 		        using (var client = new WebClient())
 		        {
 		            var response = client.DownloadString(Constanten.BING_PICTURE_OF_THE_DAY_ADDR);
-		            var obj = JsonConvert.DeserializeObject<RootObject>(response).images[0];
-		            Image = "http://www.bing.com" + obj.url;
-		            ImageCopyright = obj.copyright;
+		            var obj = JsonConvert.DeserializeObject<ImagesOfTheDayJson>(response).Images[0];
+		            Image = "http://www.bing.com" + obj.Url;
+		            ImageCopyright = obj.Copyright;
 		        }
 		    }
 		    catch
@@ -38,39 +37,80 @@ namespace LittleBitPass.Models
 		        ImageCopyright = "Night Sky";
 		    }
 		}
+		
 
-		private class IotdImage
-		{
-			public string startdate { get; set; }
-			public string fullstartdate { get; set; }
-			public string enddate { get; set; }
-			public string url { get; set; }
-			public string urlbase { get; set; }
-			public string copyright { get; set; }
-			public string copyrightlink { get; set; }
-			public bool wp { get; set; }
-			public string hsh { get; set; }
-			public int drk { get; set; }
-			public int top { get; set; }
-			public int bot { get; set; }
-			public List<object> hs { get; set; }
-		}
+        internal class ImagesOfTheDayJson
+        {
 
-		private class Tooltips
-		{
-			public string loading { get; set; }
-			public string previous { get; set; }
-			public string next { get; set; }
-			public string walle { get; set; }
-			public string walls { get; set; }
-		}
+            [JsonProperty("images")]
+            public Images[] Images { get; set; }
 
-		private class RootObject
-		{
-			public List<IotdImage> images { get; set; }
-			public Tooltips tooltips { get; set; }
-		}
+            [JsonProperty("tooltips")]
+            public Tooltips Tooltips { get; set; }
+        }
 
-	}
+        internal class Images
+        {
+
+            [JsonProperty("startdate")]
+            public string Startdate { get; set; }
+
+            [JsonProperty("fullstartdate")]
+            public string Fullstartdate { get; set; }
+
+            [JsonProperty("enddate")]
+            public string Enddate { get; set; }
+
+            [JsonProperty("url")]
+            public string Url { get; set; }
+
+            [JsonProperty("urlbase")]
+            public string Urlbase { get; set; }
+
+            [JsonProperty("copyright")]
+            public string Copyright { get; set; }
+
+            [JsonProperty("copyrightlink")]
+            public string Copyrightlink { get; set; }
+
+            [JsonProperty("wp")]
+            public bool Wp { get; set; }
+
+            [JsonProperty("hsh")]
+            public string Hsh { get; set; }
+
+            [JsonProperty("drk")]
+            public int Drk { get; set; }
+
+            [JsonProperty("top")]
+            public int Top { get; set; }
+
+            [JsonProperty("bot")]
+            public int Bot { get; set; }
+
+            [JsonProperty("hs")]
+            public object[] Hs { get; set; }
+        }
+
+        internal class Tooltips
+        {
+
+            [JsonProperty("loading")]
+            public string Loading { get; set; }
+
+            [JsonProperty("previous")]
+            public string Previous { get; set; }
+
+            [JsonProperty("next")]
+            public string Next { get; set; }
+
+            [JsonProperty("walle")]
+            public string Walle { get; set; }
+
+            [JsonProperty("walls")]
+            public string Walls { get; set; }
+        }
+
+    }
 }
 
